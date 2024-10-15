5116f3fc53d9d4d917b6e37591a6ebac985a6719
ManTran-Dev <mantmdev2001@gmail.com>
2024-10-15 20:11:14 +0700

    upload source

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      backgroundColor: {
+        main: "#151937",
+      },
+    },
+  },
+  // để sử dụng darkMode: "class"
+  darkMode: "class",
+  plugins: [require("flowbite/plugin")],
+};
